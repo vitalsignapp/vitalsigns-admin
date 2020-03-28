6c1bfb7b4a4afd2e48f891d555245be5f46da3fe
tanin5462 <ber_zebub@hotmail.com>
2020-03-28 11:02:27 +0700

    Connected With Firebase

--- src/router/index.js
+++ src/router/index.js
@@ -14,9 +14,27 @@ Vue.use(VueRouter)
  * with the Router instance.
  */
 
-export default function (/* { store, ssrContext } */) {
+var firebaseConfig = {
+  apiKey: "AIzaSyDkfuz9optU8t14BZJBgJ9JNYdH4Omdh6A",
+  authDomain: "vitalsign-2bc48.firebaseapp.com",
+  databaseURL: "https://vitalsign-2bc48.firebaseio.com",
+  projectId: "vitalsign-2bc48",
+  storageBucket: "vitalsign-2bc48.appspot.com",
+  messagingSenderId: "67633726727",
+  appId: "1:67633726727:web:b535d92a91ec80695bb1a2",
+  measurementId: "G-MEX9V112SR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore();
+
+
+export default function ( /* { store, ssrContext } */ ) {
   const Router = new VueRouter({
-    scrollBehavior: () => ({ x: 0, y: 0 }),
+    scrollBehavior: () => ({
+      x: 0,
+      y: 0
+    }),
     routes,
 
     // Leave these as they are and change in quasar.conf.js instead!
