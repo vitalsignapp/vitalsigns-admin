de76204191afbf2673ff25c41264d503e1f12691
v_surasakk <people29@hotmail.com>
2020-04-12 00:50:58 +0700

    add interpretor axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,31 @@
 import axios from "axios";
+import { Cookies } from 'quasar';
 
 const host = process.env.API_HOST;
-const jsonConfig = {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-};
+const httpClient = axios.create({ baseURL : host });
+
+httpClient.interceptors.request.use(async (config) => {
+  const accessToken = Cookies.get("access-token");
+  config.headers.Authorization = `Bearer ${accessToken}`;
+  return config;
+});
 
 export function login({email, password}) {
   const url = `${host}/login`;
-  return axios.post(url, 
-    {
-      email: email, 
-      password: password
-    }, jsonConfig)
-    .then(response => response.data)
-    .catch(handleError);
+  return axios.post(url, { email: email, password: password })
+    .then(response => {
+      if (response.data) {
+        return axios.get(`${host}/auth`).then(res => {
+          Cookies.set('access-token', res.data.token, {expires: '15m'}); //expired 15 minutes
+          return response.data;
+        });
+      } 
+    }).catch(handleError);
 }
 
 export function listRoom(hospitalId) {
-  const url = `${host}/ward/${hospitalId}`;
-  return axios.get(url, { withCredentials: true })
+  const url = `/ward/${hospitalId}`;
+  return httpClient.get(url)
     .then(response => response.data)
     .catch(handleError);
 }
