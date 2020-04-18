1aae5dd1e968e31b55fe36b51f70d29d62aa8b9b
supagrid tangsermsit <boyisboyis.work@gmail.com>
2020-04-19 00:24:49 +0700

    build: add linter

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  plugins: [
+    "quasar",
+    "import",
+  ],
+  rules: {
+    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }],
+
+    'vue/max-attributes-per-line': 0,
+    'vue/valid-v-for': 0,
+
+    // allow async-await
+    'generator-star-spacing': 'off',
+
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    'one-var': 0,
+    'quotes': ['error', 'single'],
+    "prettier/prettier": ["error", { "singleQuote": true }],
+    'import/first': 0,
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
--- quasar.conf.js
+++ quasar.conf.js
@@ -72,6 +72,15 @@ module.exports = function (ctx) {
           // Add your own alias like this
           '@': path.resolve(__dirname, './src'),
         }
+        /*cfg.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /[\\/]node_modules[\\/]/,
+          options: {
+            formatter: require('eslint').CLIEngine.getFormatter('stylish')
+          }
+        })*/
       }
     },
 
