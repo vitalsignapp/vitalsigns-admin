bef2ccc19d31353155efaa915d1e3e6fbe0a9bfe
supagrid tangsermsit <boyisboyis.work@gmail.com>
2020-04-19 11:28:44 +0700

    fix: linter

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const API_HOST = process.env.API_HOST;
 const FIREBASE_CONFIG = {
   apiKey: 'AIzaSyDkfuz9optU8t14BZJBgJ9JNYdH4Omdh6A',
@@ -17,22 +16,3 @@ export default {
   API_HOST,
   FIREBASE_CONFIG,
 };
-=======
-
-const API_HOST = process.env.API_HOST;
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDkfuz9optU8t14BZJBgJ9JNYdH4Omdh6A",
-  authDomain: "vitalsign-2bc48.firebaseapp.com",
-  databaseURL: "https://vitalsign-2bc48.firebaseio.com",
-  projectId: "vitalsign-2bc48",
-  storageBucket: "vitalsign-2bc48.appspot.com",
-  messagingSenderId: "67633726727",
-  appId: "1:67633726727:web:b535d92a91ec80695bb1a2",
-  measurementId: "G-MEX9V112SR"
-};
-
-export default {
-  API_HOST,
-  FIREBASE_CONFIG,
-}
->>>>>>> feat: add path alias
