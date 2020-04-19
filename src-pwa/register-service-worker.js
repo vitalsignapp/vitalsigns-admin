74854a959335b8bc2965fd2a1a26d7bf986603f8
supagrid tangsermsit <boyisboyis.work@gmail.com>
2020-04-19 12:28:58 +0700

    fix: linter

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -1,6 +1,4 @@
-import {
-  register
-} from 'register-service-worker'
+import { register } from 'register-service-worker';
 
 // The ready(), registered(), cached(), updatefound() and updated()
 // events passes a ServiceWorkerRegistration instance in their arguments.
@@ -15,44 +13,46 @@ register(process.env.SERVICE_WORKER_FILE, {
 
   ready() {
     if (process.env.DEV) {
-      console.log('App is being served from cache by a service worker.')
+      console.log('App is being served from cache by a service worker.');
     }
   },
 
-  registered( /* registration */ ) {
+  registered(/* registration */) {
     if (process.env.DEV) {
-      console.log('Service worker has been registered.')
+      console.log('Service worker has been registered.');
     }
   },
 
-  cached( /* registration */ ) {
+  cached(/* registration */) {
     if (process.env.DEV) {
-      console.log('Content has been cached for offline use.')
+      console.log('Content has been cached for offline use.');
     }
   },
 
-  updatefound( /* registration */ ) {
+  updatefound(/* registration */) {
     if (process.env.DEV) {
-      console.log('New content is downloading.')
+      console.log('New content is downloading.');
     }
   },
 
-  updated( /* registration */ ) {
+  updated(/* registration */) {
     if (process.env.DEV) {
-      console.log('New content is available; please refresh.')
-      window.location.reload(true)
+      console.log('New content is available; please refresh.');
+      window.location.reload(true);
     }
   },
 
   offline() {
     if (process.env.DEV) {
-      console.log('No internet connection found. App is running in offline mode.')
+      console.log(
+        'No internet connection found. App is running in offline mode.'
+      );
     }
   },
 
   error(err) {
     if (process.env.DEV) {
-      console.error('Error during service worker registration:', err)
+      console.error('Error during service worker registration:', err);
     }
-  }
-})
+  },
+});
